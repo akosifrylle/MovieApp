983c63d2eac2986505bd2f326f60e764d79d6cdd
Frylle Bancaso <frylle.bancaso@cormanttech.com>
2014-12-15 10:44:04 +0800

    Set up Fluent NHibernate

---
+++ MovieDB.NHibernate.SqlServer/Maps/GenreMap.cs
@@ -0,0 +1,10 @@
+﻿using FluentNHibernate.Mapping;
+using MovieDB.NHibernate.Repo;
+
+namespace MovieDB.NHibernate.SqlServer.Maps
+{
+    public class GenreMap : ClassMap<GenreRepo>
+    {
+
+    }
+}
--- MovieDB.NHibernate/Dao/MovieDao.cs
+++ MovieDB.NHibernate/Dao/MovieDao.cs
@@ -2,7 +2,31 @@
 
 namespace MovieDB.NHibernate.Dao
 {
-    public class MovieDao : Imovie
+    public class MovieDao : IMovieDao
     {
+        public Movie AddMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IList<Movie> GetByGenre(string genre)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IList<Movie> GetByProductionCompany(string productionCompany)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public System.Collections.Generic.IList<Movie> GetList()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
